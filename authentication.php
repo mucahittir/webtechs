975ab489740f4371988de93455d14377d75af238
mucahittir <mucahittiryaki11@gmail.com>
2021-05-21 11:07:17 +0300

    Genel Düzenlemeler, login sayfası düzenlemesi, hata düzeltmeleri

--- authentication.php
+++ authentication.php
@@ -1,10 +1,11 @@
 <?php
-$ogrno = "g171210008";
-$sifre = "22994214676";
-if (!isset($_POST['kullanici']) || !isset($_POST['sifre'])) {
-    echo "Hatalı giriş yaptınız";
-    return null;
-}
-if ($ogrno == $_POST['kullanici'] || $sifre == $_POST['sifre']) {
-    echo "Harikalar yaratıyorsunuz hoşgeldiniz " . $_POST['kullanici'];
+$ogrno = "g171210008@sakarya.edu.tr";
+$sifre = "g171210008";
+$isim = "Mücahit Tiryaki";
+if ($ogrno == $_POST['kullanici'] && $sifre == $_POST['sifre']) {
+    header("HTTP/1.1 200 OK");
+    echo "Hoşgeldiniz " . $isim;
+} else {
+    header("HTTP/1.1 403 Forbidden");
+    echo "Hatalı giriş yaptınız.";
 }
